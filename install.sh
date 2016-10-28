4813a1ddd1dd1b313b4126eb764837cd22dfdaa8
Sean Heelan <sean@vertex.re>
2016-10-28 15:11:34 +0100

    Terminator config

--- install.sh
+++ install.sh
@@ -16,4 +16,8 @@ cat shell/aliases >> $HOME/.bashrc
 echo "Installing bashrc extensions ..."
 cat shell/bashrc >> $HOME/.bashrc
 
+echo "Installing terminator config ..."
+mkdir -p ~/.config/terminator/                                                   
+cp shell/terminator_config ~/.config/terminator/config
+
 echo "Done"
